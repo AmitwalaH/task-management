66e843d86f9278131ce10e2d49545c2e4ad14780
AmitwalaH <algoworks818@gmail.com>
2026-02-06 19:23:46 +0530

    Implement the basic App for our task management

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Import pages
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Tasks from "./pages/Tasks";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Redirect root to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Auth pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Main app pages */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:projectId/tasks" element={<Tasks />} />
+        <Route path="/profile" element={<Profile />} />
+
+        {/* 404 page */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
